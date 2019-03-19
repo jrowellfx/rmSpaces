d4aba22e71cc07ade03005fb8727991fc56abc43
James Philip Rowell <james.rowell@yahoo.com>
2019-03-19 11:05:11 -0500

    Added files to install on system and fixed usage of 'tac' that is not available on macos unix.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.7
+
+from distutils.core import setup
+import os
+import shutil
+
+setup(name='rmSpaces',
+      version='1.000',
+      description='Script to walk tree and remove spaces from filenames.',
+      long_description='',
+      author='James Philip Rowell',
+      author_email='james@alpha-eleven.com',
+      url='http://www.alpha-eleven.com/',
+      py_modules=[],
+      scripts=['rmSpaces'],
+      license = "BSD 3-Clause license",
+     )
